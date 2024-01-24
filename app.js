e0954acbc575ac43baf5e13f897eb2d09335e6d8
PRESISPREMACHANDRAN <presipremachandran@gmail.com>
2024-01-24 13:52:01 +0530

    development branch

---
+++ app.js
@@ -0,0 +1,14 @@
+const express=require("express")
+const cors=require("cors")
+const bodyParser=require("body-parser")
+
+const app=express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cors())
+
+
+app.listen(4000,()=>{
+    console.log("server starts running....")}
+)
